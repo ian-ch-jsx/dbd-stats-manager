9570a45c3859270b3af44ad4a949ec83efef16f5
Ian Christiansen <ianc.songdog@gmail.com>
2022-05-24 11:26:46 -0700

    wrote survivor stat tracking service calls for update and create

--- src/services/data.js
+++ src/services/data.js
@@ -14,3 +14,19 @@ export async function getSurvivorPerkById(ID) {
   const resp = await client.from('survivor_perks').select('*').match({ ID }).single();
   return checkError(resp);
 }
+
+//update stats
+export async function updateSurvivorStatsById({ id, perk_id, wins, losses, user_id }) {
+  const resp = await client
+    .from('survivor_stats')
+    .update({ perk_id, wins, losses })
+    .match({ id })
+    .eq('user.id', user_id);
+  return checkError(resp);
+}
+
+//create stats
+export async function createSurvivorStatsById({ perk_id, wins, losses, user_id }) {
+  const resp = await client.from('survivor_stats').insert({ perk_id, wins, losses, user_id });
+  return checkError(resp);
+}
