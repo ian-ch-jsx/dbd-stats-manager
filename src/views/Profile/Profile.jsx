e7cf14af973071fd8396031bf5432798f6b57b66
Ian Christiansen <ianc.songdog@gmail.com>
2022-05-26 11:27:35 -0700

    fetching user data for stats page

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useUser } from '../../context/UserContext';
+import { getKillerPerks, getSurvivorPerks } from '../../services/data';
+import { getKillerStatsByUserId } from '../../services/stats';
 
 export default function Profile() {
   const { user } = useUser();
+  const [topKiller, setTopKiller] = useState([]);
+  const [topSurvivor, setTopSurvivor] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return <div>Profile</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const survivorPerks = await getSurvivorPerks();
+      const killerPerks = await getKillerPerks();
+      const data = await getKillerStatsByUserId(user.id);
+      setTopKiller(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      TEST <p>{topKiller.perk_id}</p>
+    </div>
+  );
 }
